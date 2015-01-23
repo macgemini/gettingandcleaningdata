039e25edbed672d9bc3f12f6ea1f8676d0a2f18b
macgemini <gememacks@gmail.com>
2015-01-23 14:53:26 +0100

    initial commit

---
+++ run_analysis.R
@@ -0,0 +1,48 @@
+library(RCurl)
+
+if (!file.info('UCI HAR Dataset')$isdir) {
+  dataFile <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+  dir.create('UCI HAR Dataset')
+  download.file(dataFile, 'UCI-HAR-dataset.zip', method='curl')
+  unzip('./UCI-HAR-dataset.zip')
+}
+
+
+x.train <- read.table('./UCI HAR Dataset/train/X_train.txt')
+x.test <- read.table('./UCI HAR Dataset/test/X_test.txt')
+x <- rbind(x.train, x.test)
+
+subj.train <- read.table('./UCI HAR Dataset/train/subject_train.txt')
+subj.test <- read.table('./UCI HAR Dataset/test/subject_test.txt')
+subj <- rbind(subj.train, subj.test)
+
+y.train <- read.table('./UCI HAR Dataset/train/y_train.txt')
+y.test <- read.table('./UCI HAR Dataset/test/y_test.txt')
+y <- rbind(y.train, y.test)
+
+ 
+features <- read.table('./UCI HAR Dataset/features.txt')
+mean.sd <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+x.mean.sd <- x[, mean.sd]
+
+names(x.mean.sd) <- features[mean.sd, 2]
+names(x.mean.sd) <- tolower(names(x.mean.sd)) 
+names(x.mean.sd) <- gsub("\\(|\\)", "", names(x.mean.sd))
+
+activities <- read.table('./UCI HAR Dataset/activity_labels.txt')
+activities[, 2] <- tolower(as.character(activities[, 2]))
+activities[, 2] <- gsub("_", "", activities[, 2])
+
+y[, 1] = activities[y[, 1], 2]
+colnames(y) <- 'activity'
+colnames(subj) <- 'subject'
+
+# label
+data <- cbind(subj, x.mean.sd, y)
+str(data)
+write.table(data, 'tidy_mer.txt')
+
+# average values
+average <- aggregate(x=data, by=list(activities=data$activity, subj=data$subject), FUN=mean)
+str(average)
+write.table(average, 'tidy_av.txt')
